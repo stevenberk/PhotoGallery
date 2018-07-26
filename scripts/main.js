0e39fbaedb9170e70c3e475a18e074a7afe7b670
Steven Berk <stevenberk@Stevens-MacBook-Pro.local>
2018-07-26 09:00:47 -0400

    index values added to photo array

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,21 @@
 var myPics = [
   { caption: "Big Antique Store", url: "images/antiqueStore.jpg" },
   { caption: "Inle Lake", url: "images/lake.jpg" },
-  { caption: "Mt Popa at Golden Hour", url: "images/sunset.jpg" },
-  { caption: "Yeah I travel to places", url: "images/bagan.jpg", },
-  { caption: "SEC Sportsball Fan", url: "images/SECfan.jpg"}
+  { caption: "Mt Popa at Golden Hour", url: "images/sunset.jpg"  },
+  { caption: "Yeah I travel to places", url: "images/bagan.jpg" },
+  { caption: "SEC Sportsball Fan", url: "images/SECfan.jpg" }
 ];
 var lightBox = document.querySelector('.lightBox'); // div tag(set to display none as default)
 var lightBoxImage = document.querySelector('.lightBoxImage'); //image tag
 var container = document.querySelector('.theImages'); //ul tag 
 
-var lightBoxOn = function (event) {
-  lightBox.setAttribute("class", 'lightBoxOn')
-  lightBoxImage.setAttribute("src", (event.target.src)) // to the image tag
 
+var lightBoxOn = function (event) {
+  var index = event.target.getAttribute("data-index")
+  var image = myPics[index] 
+  lightBox.setAttribute("class", 'lightBoxOn')
+  lightBoxImage.setAttribute("src", (image.url)) // to the image tag
+  console.log(event);
 }
 
 var lightBoxOff = function(){   //this actually turns OFF the lightbox
@@ -20,13 +23,16 @@ var lightBoxOff = function(){   //this actually turns OFF the lightbox
   
 }
 
-for (var image of myPics) {
+for (var index = 0; index < myPics.length; index++) {
+
+var image = myPics[index]
   var newImage = document.createElement('img');
   newImage.setAttribute('src', image.url );
   newImage.classList.add('image');
-  
+  newImage.setAttribute('data-index', index);
+
   var imageContainer = document.createElement('div');
-  imageContainer.classList.add('pics')
+  imageContainer.classList.add('pics');
 
   var caption = document.createElement('p');
   caption.textContent = image.caption;
@@ -34,11 +40,15 @@ for (var image of myPics) {
   var listItem = document.createElement('li');
   listItem.classList.add('listItem');
 
+
+ 
+
   imageContainer.appendChild(newImage);
   listItem.appendChild(imageContainer);
   listItem.appendChild(caption);
   imageContainer.addEventListener('click', lightBoxOn);
   container.appendChild(listItem);
+  
 }
 lightBox.addEventListener("click", lightBoxOff);
 
